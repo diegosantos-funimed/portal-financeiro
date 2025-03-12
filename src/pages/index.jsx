721b943432d716e4521de45946d48964469af748
diegosantos7 <diegosantos@faculdadeunimed.edu.br>
2025-03-12 11:21:44 -0300

    update(): update env yml

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ export default function DashboardPage() {
           {
             method: "POST",
             headers: {
-              "Authorization": `Basic ${process.env.API_KEY}`,
+              "Authorization": `Basic ${process.env.NEXT_PUBLIC_API_KEY}`,
               "User-Agent": "insomnia/10.3.0",
               "X-Explorer-Account-Token": "faculdadeunimed-dev"
             },
